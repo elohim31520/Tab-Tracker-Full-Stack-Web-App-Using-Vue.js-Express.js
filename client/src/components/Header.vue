6cc7391472fc0bf935d19881a02f8349f0a5974e
Hsin Yu <versus7063@gmail.com>
2018-09-05 15:01:37 +0800

    環境設定

---
+++ client/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+    v-toolbar(dark,fixed).cyan
+        v-toolbar-title.mr-4
+            router-link(tag="span",:to="{name: 'songs'}").home  Tab Tracker
+        v-toolbar-items
+            v-btn(dark,flat).cyan Browse
+        v-spacer
+        v-toolbar-items
+            v-btn(dark,flat,:to="{ name: 'register'}").cyan Sign up
+            v-btn(dark,flat,:to="{ name: 'login'}").cyan Login
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang='sass'>
+.home
+    cursor: pointer
+    &:hover
+        color: #000
+</style>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template lang='pug'>
+    v-layout
+        v-flex(xs6,offset-xs3)
+            .white.elevation-2
+                v-toolbar.cyan(dark,flat,dense)
+                    v-toolbar-title Login
+
+                .px-4.py-2
+                    v-text-field(label="Email",v-model="email",:rules="[rules.required, rules.email]")
+                    br
+                    v-text-field(label="Password",v-model="password",:rules="[rules.required]")
+                    br
+                    v-btn.cyan(dark) Login
+
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            email:'',
+            password: '',
+            rules: {
+                required: value => !!value || 'Required.',
+                counter: value => value.length <= 20 || 'Max 20 characters',
+                email: value => {
+                    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    return pattern.test(value) || 'Invalid e-mail.'
+                    }
+            }
+        }
+    
+    }
+}
+</script>
+
+<style>
+
+</style>
